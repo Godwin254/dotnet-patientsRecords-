0970256cffe4ccd46c92053b9049f2dc8756c5bb
Godwin254 <godwinbee15@gmail.com>
2022-10-15 16:37:59 +0300

    new: initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace patientsRecords.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
